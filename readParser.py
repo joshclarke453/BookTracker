2b2e0d378bded37bc9b462ed282f4c94fa81153a
Josh <joshclarke453@gmail.com>
2024-04-06 12:55:15 -0230

    Parser

---
+++ readParser.py
@@ -0,0 +1,19 @@
+import json
+f = open('./read.txt', encoding='utf-8')
+lines = f.readlines()
+bookObjs = []
+bookObj = []
+composite_list = [lines[x:x+12] for x in range(0, len(lines),12)]
+objList = []
+for x in composite_list:
+    obj = {
+        "Title": x[1].replace('\n', ''),
+        "Author": x[2].replace('\n', ''),
+        "Read Date": x[7].replace('\n', '').replace('not set', '').replace(' [edit]', ''),
+        "Owned": True,
+        "Read": x[7] != 'not set [edit]\n'
+    }
+    if "bookcase" not in x[5]:
+        objList.append(obj)
+with open('./read.json', 'w', encoding='utf-8') as f:
+    json.dump(objList, f, ensure_ascii=False, indent=4)
